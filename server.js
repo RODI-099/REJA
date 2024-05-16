38ed02c4b27ecb82c1d367663f49cbbc675dc754
RODI-099 <rodiiy099@gmail.com>
2024-05-16 09:44:59 +0500

    feat: develop author router ejs  page

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
-// const res = require("express/lib/response");
 const app = express();
 const http = require("http");
 const fs = require("fs");
 
 let user;
+
 fs.readFile("database/user.json", "utf8", (err, data) => {
     if(err) {
         console.log("ERROR:", err);
     } else {
         user = JSON.parse(data);
     }
-})
+});
+
+
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -39,7 +41,7 @@ app.post("/create-item", (req, res) => {
     res.json({test: "success"});
 });
 
-app.get('/author', (req, res) => {
+app.get("/author", (req, res) => {
     res.render("author", {user: user});
 })
 app.get("/", function(req, res) {
@@ -51,3 +53,4 @@ let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
+
