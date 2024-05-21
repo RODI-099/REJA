4be2343d5975043c04eb5157c7ab4972f56c6b2c
RODI-099 <rodiiy099@gmail.com>
2024-05-21 08:52:46 +0500

    feat: train callback and async functions

--- train.js
+++ train.js
@@ -1,1 +1,100 @@
-console.log('train task ishga tushdi');
+
+// A-TASK: 
+
+// Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
+function countLetter(letter, word) {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countLetter("e", "example")); // Output: 3
+console.log(countLetter("n", "driver")); // Output: 2
+console.log(countLetter("x", "carpenter")); // Output: 0
+
+
+// console.log('train task ishga tushdi');
+// console.log("Jack Ma maslahatlari");
+// const list = [
+// "yahshi talaba boling",
+// "togri boshliq tanlang va koproq hato qiling", // 20-30
+// "uzingizga ishlashingizni boshlang", // 30-40
+// "siz kuchli bolgan narsalarni qiling", // 40-50
+// "yoshlarga investitsiya qiling",
+// // 50-60
+// "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+// CALLBACK function
+// function maslahatBering(a, callback) {
+//     if(typeof a !== "number") callback("insert a number", null);
+//     else if (a <= 20) callback(null,list[0]);
+//     else if(a > 20 && a <= 30) callback(null, list[1]);
+//     else if(a > 30 && a <= 30) callback(null, list[2]);
+//     else if(a > 40 && a <= 30) callback(null, list[3]);
+//     else if(a > 50 && a <= 30) callback(null, list[4]);
+//     else {
+//         setInterval(function () {
+//        callback(null, list[5]);
+//     }, 1000);
+// }
+    
+// }
+// console.log("passed here 0");
+// maslahatBering(25, (err, data) => {
+//     if(err) console.log('ERROR:', err);
+//     else {
+//     console.log("javob:",data);
+//     }
+// } );
+// console.log("passed here 1");
+
+// ASYNC FUNCTION
+// async function maslahatBering(a) {
+//     if(typeof a !== "number") throw new Error("insert a number");
+//     else if (a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setInterval(() => {
+
+                //      setTimeout(() => {
+        //         resolve(list[5]);
+        //     }, 1000);
+        // });
+    //    return list[5];
+    //     setTimeout(function () {
+    //    return list[5];
+    // }, 5000);
+// }
+    
+// }
+// call via then/catch
+// console.log("passed here 0");
+// maslahatBering(20)
+// .then(data => {
+//     console.log("javob:", data);
+// })
+// .catch(err => {
+//     console.log("ERROR:", err);
+// });
+// console.log("passed here 1");
+
+// async/await
+// async function run() {
+//     let javob = await maslahatBering(65);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
