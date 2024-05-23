4cb03b1f9ab66b241f16c1410df1f958f782ac51
RODI-099 <rodiiy099@gmail.com>
2024-05-23 09:53:02 +0500

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,71 +1,29 @@
-console.log("Web Serverni boshlash");
-const {log} = require("console");
-const express = require("express");
-const app = express();
-const res = require("express/lib/response")
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
+let db;
+const connectionString = "mongodb+srv://RODI-99:Quantum99@cluster0.31rebwt.mongodb.net/REJA"
 
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if (err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data);
-
-        // Start the server only after user data is loaded
-    
-    }
-});
- 
-
-// 1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2: Session code 
-
-// 3 Views code 
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-
-// 4 Routing code
-// app.get("/hello", function(req, res) {
-//     res.end(`<h1>HELLO WORLD</h1>`);
-// });
-// app.get("/gift", function(req, res) {
-//     res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-// });
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
-});
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-})
-app.get("/", function(req, res) {
-    res.render("reja")
-});
-
-const server = http.createServer(app);
+mongodb.connect(
+    connectionString,
+     {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succed");
+        module.exports = client;
+        
+        const app = require("./app");
+        const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+);
 
-    );
 });
+    }
+}
+);
 
-// Yuqorida Express orqali web server qurib oldik!
-// EJS orqali backend ichida frontend ni qurib olamz!
-
-// Nodemon -- bu package kodda o'zgarish bo'lsa serverni qayta ishga tushiradi!
-// npm start => faqat start script bo'lsagina run beradi.
-// npm run start => start dan boshqa script lar bo'lsa masalan: dev, bu holatda npm run dev ishlatamz!
-
-//  GET - database dan ma'lumotni olish uchun GET ishlatiladi.
-// POST - ma'lumotni o'zi bilan olib keladi va database ga o'sha ma'lumotni yozadi.
