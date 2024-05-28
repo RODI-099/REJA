5e563d3186b79f6f30e5c09fa1a02425e539ec4d
RODI-099 <rodiiy099@gmail.com>
2024-05-28 17:21:49 +0500

    modified train.js

--- train.js
+++ train.js
@@ -1,4 +1,8 @@
-// 
+// D-TASK: 
+
+// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+
 
 function SameLetters(str1, str2) {
     const sortedStr1 = str1.split('').sort().join('');
